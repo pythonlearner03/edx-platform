46596bcd3727590b794ea2a262ac78b160b9ddfe
Michael Roytman <mroytman@edx.org>
2018-07-27 11:44:53 -0400

    bok choy tests for course outline status section

--- common/test/db_cache/bok_choy_migrations_data_student_module_history.sql
+++ common/test/db_cache/bok_choy_migrations_data_student_module_history.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.32, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Host: localhost    Database: student_module_history_test
+-- Host: edx.devstack.mysql    Database: student_module_history_test
 -- ------------------------------------------------------
--- Server version	5.6.32-1+deb.sury.org~xenial+0.1
+-- Server version	5.6.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-20 11:18:00
+-- Dump completed on 2018-07-27 15:39:14
